066602a3c6a5382969b6f49ffaa2e8264cc97cd2
Alice <amyalicedee@gmail.com>
2016-07-11 22:47:11 +0100

    Help!

--- src/apirequestfetch.js
+++ src/apirequestfetch.js
@@ -1,4 +1,4 @@
-var fetch = require("node-fetch");
+var fetch = require("fetch");
 
 function apirequestfetch(userIP) {
   var url =  "http://api.openweathermap.org/data/2.5/weather?lat=51.5&lon=-0.47&appid=b916b345d07747cfa00b95973acb1c2a"
