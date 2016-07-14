7524ccaf5242b3f6171a95b9f1f62ae8874133a1
Alice <amyalicedee@gmail.com>
2016-07-14 12:06:44 +0100

    Add background image of weather to views

--- src/apirequestfetch.js
+++ src/apirequestfetch.js
@@ -11,9 +11,65 @@ function getAndShowWeather(position) {//takes fetch in here somewhere?
   var lon = position.coords.longitude;
   apirequestfetch(lat, lon)
     .then(function(weatherObj) {
-      console.log(weatherObj.weather[0].description);
+      // console.log(weatherObj.weather[0].description);
+      document.querySelector("#greet").innerHTML = "The weather in the environs of lovely " + weatherObj.name + " is alleged, at present, to be:";
       document.querySelector("#description").innerHTML = weatherObj.weather[0].description;
-      document.querySelector("#icon").src = "http://openweathermap.org/img/w/" + weatherObj.weather[0].icon + ".png"
+      document.querySelector("#icon").src = "http://openweathermap.org/img/w/" + weatherObj.weather[0].icon + ".png";
+        if (weatherObj.weather[0].icon === "01d") {
+          document.querySelector(".background").id = "clear-day"
+        };
+        if (weatherObj.weather[0].icon === "01n") {
+          document.querySelector(".background").id = "clear-night"
+        }
+        if (weatherObj.weather[0].icon === "02d") {
+          document.querySelector(".background").id = "few-clouds-day"
+        }
+        if (weatherObj.weather[0].icon === "02n") {
+          document.querySelector(".background").id = "few-clouds-night"
+        }
+        if (weatherObj.weather[0].icon === "03d") {
+          document.querySelector(".background").id = "scattered-clouds-day"
+        }
+        if (weatherObj.weather[0].icon === "03n") {
+          document.querySelector(".background").id = "scattered-clouds-night"
+        }
+        if (weatherObj.weather[0].icon === "04d") {
+          document.querySelector("body").id = "broken-clouds-day"
+        }
+        if (weatherObj.weather[0].icon === "04n") {
+          document.querySelector(".background").id = "broken-clouds-night"
+        }
+        if (weatherObj.weather[0].icon === "09d") {
+          document.querySelector(".background").id = "shower-rain-day"
+        }
+        if (weatherObj.weather[0].icon === "09n") {
+          document.querySelector(".background").id = "shower-rain-night"
+        }
+        if (weatherObj.weather[0].icon === "10d") {
+          document.querySelector(".background").id = "rain-day"
+        }
+        if (weatherObj.weather[0].icon === "10n") {
+          document.querySelector(".background").id = "rain-night"
+        }
+        if (weatherObj.weather[0].icon === "11d") {
+          document.querySelector(".background").id = "thunderstorm-day"
+        }
+        if (weatherObj.weather[0].icon === "11n") {
+          document.querySelector(".background").id = "thunderstorm-night"
+        }
+        if (weatherObj.weather[0].icon === "13d") {
+          document.querySelector(".background").id = "snow-day"       
+        }
+        if (weatherObj.weather[0].icon === "13n") {
+          document.querySelector(".background").id = "snow-night"
+        }
+        if (weatherObj.weather[0].icon === "50d") {
+          document.querySelector(".background").id = "mist-day"       
+        }
+        if (weatherObj.weather[0].icon === "50n") {
+          document.querySelector(".background").id = "mist-night"      
+        }
+        
     });
 };
 
